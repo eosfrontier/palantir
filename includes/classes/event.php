8403f47a46569c03d3de1205f90e41da1c882d9b
WafflesMcDuff <goldjd@gmail.com>
2020-11-22 20:22:03 -0500

    Add location_address to fields exported

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -3,23 +3,24 @@
 class event {
 
 	public function get_all() {
-		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location,e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
-		join la_eb_locations loc on loc.id = e.location_id
-        join la_users usr on usr.id = e.created_by
-		WHERE e.published = 1" );
+		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location_name, loc.address as location_address,
+		e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
+				join la_eb_locations loc on loc.id = e.location_id
+				join la_users usr on usr.id = e.created_by
+				WHERE e.published = 1" );
 		$res  = $stmt->execute();
 		$res  = $stmt->fetchAll( PDO::FETCH_ASSOC );
 		return $res;
 	}
 
 	public function get( $id, $needle ) {
-			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location,e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
-			join la_eb_locations loc on loc.id = e.location_id
-			join la_users usr on usr.id = e.created_by
+			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location_name, loc.address as location_address,
+			e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
+					join la_eb_locations loc on loc.id = e.location_id
+					join la_users usr on usr.id = e.created_by
 			WHERE e.published = 1 AND e.id = $id" );
 			$res  = $stmt->execute( [ $id ] );
 			$res  = $stmt->fetch( PDO::FETCH_ASSOC );
-
 		return $res;
 	}
 }
