bba2cd523b1870e6e2dc19a996fe741ff6431924
WafflesMcDuff <goldjd@gmail.com>
2020-11-22 20:27:26 -0500

    Add orga to exported data

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -3,10 +3,12 @@
 class event {
 
 	public function get_all() {
-		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location_name, loc.address as location_address,
-		e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
+		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
+		loc.address as location_address, e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, 
+		e.registration_start_date FROM la_eb_events e
 				join la_eb_locations loc on loc.id = e.location_id
 				join la_users usr on usr.id = e.created_by
+				join la_eb_categories orga on orga.id = e.main_category_id
 				WHERE e.published = 1" );
 		$res  = $stmt->execute();
 		$res  = $stmt->fetchAll( PDO::FETCH_ASSOC );
@@ -14,10 +16,12 @@ class event {
 	}
 
 	public function get( $id, $needle ) {
-			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location_name, loc.address as location_address,
-			e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
+			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
+			loc.address as location_address, e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, 
+			e.registration_start_date FROM la_eb_events e
 					join la_eb_locations loc on loc.id = e.location_id
 					join la_users usr on usr.id = e.created_by
+					join la_eb_categories orga on orga.id = e.main_category_id
 			WHERE e.published = 1 AND e.id = $id" );
 			$res  = $stmt->execute( [ $id ] );
 			$res  = $stmt->fetch( PDO::FETCH_ASSOC );
