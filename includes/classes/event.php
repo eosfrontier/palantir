c5b48ae5936a246d0481575219a186ca0c2d04a7
WafflesMcDuff <goldjd@gmail.com>
2020-11-22 20:04:39 -0500

    Update event.php

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -16,7 +16,7 @@ class event {
 			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, e.event_date, e.event_end_date, loc.name as location,e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, e.registration_start_date FROM la_eb_events e
 			join la_eb_locations loc on loc.id = e.location_id
 			join la_users usr on usr.id = e.created_by
-			WHERE e.published = 1 AND $needle = ?" );
+			WHERE e.published = 1 AND e.id = $id" );
 			$res  = $stmt->execute( [ $id ] );
 			$res  = $stmt->fetch( PDO::FETCH_ASSOC );
 
