ae8ec5faad17afc26ec2432efc2292b65abfbf71
WafflesMcDuff <goldjd@gmail.com>
2020-11-22 20:30:42 -0500

    Update index.php

--- api/v1/event/index.php
+++ api/v1/event/index.php
@@ -4,15 +4,15 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/palantir/includes/token.php';
 $c_fetch = new event();
 
 switch ( $method ) {
-	case 'DELETE':
-		require_once './_delete.php';
-		break;
-	case 'POST':
-		require_once './_post.php';
-		break;
-	case 'PUT':
-		require_once './_update.php';
-		break;
+	// case 'DELETE':
+	// 	require_once './_delete.php';
+	// 	break;
+	// case 'POST':
+	// 	require_once './_post.php';
+	// 	break;
+	// case 'PUT':
+	// 	require_once './_update.php';
+	// 	break;
 	case 'GET':
 		require_once './_get.php';
 		break;
