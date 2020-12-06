f715cb9e7e65de9064136db470c25571e3c0fb96
WafflesMcDuff <goldjd@gmail.com>
2020-12-05 22:28:37 -0500

    parse custom_fields as separate objects

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -4,11 +4,18 @@ class event {
 
 	public function get_all() {
 		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
-		loc.address as location_address, e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, 
+		loc.address as location_address, e.short_description, e.short_description_en, 
+		replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangments, 
+		replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre, 
+		replace(json_extract(e.custom_fields,'$.genre2'),'\"','') as genre2,
+		replace(json_extract(e.custom_fields,'$.genre3'),'\"','') as genre3,
+		replace(json_extract(e.custom_fields,'$.min_age'),'\"','') as min_age,
+		replace(json_extract(e.custom_fields,'$.max_age'),'\"','') as max_age,
+		e.price_text, 
 		e.registration_start_date, e.registration_handle_url FROM la_eb_events e
-				join la_eb_locations loc on loc.id = e.location_id
-				join la_users usr on usr.id = e.created_by
-				join la_eb_categories orga on orga.id = e.main_category_id
+				left join la_eb_locations loc on loc.id = e.location_id
+				left join la_users usr on usr.id = e.created_by
+				left join la_eb_categories orga on orga.id = e.main_category_id
 				WHERE e.published = 1" );
 		$res  = $stmt->execute();
 		$res  = $stmt->fetchAll( PDO::FETCH_ASSOC );
@@ -17,12 +24,19 @@ class event {
 
 	public function get( $id, $needle ) {
 			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
-			loc.address as location_address, e.short_description, e.short_description_en, usr.name as created_by, e.custom_fields, e.price_text, 
+			loc.address as location_address, e.short_description, e.short_description_en, 
+			replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangments, 
+			replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre, 
+			replace(json_extract(e.custom_fields,'$.genre2'),'\"','') as genre2,
+			replace(json_extract(e.custom_fields,'$.genre3'),'\"','') as genre3,
+			replace(json_extract(e.custom_fields,'$.min_age'),'\"','') as min_age,
+			replace(json_extract(e.custom_fields,'$.max_age'),'\"','') as max_age,
+			e.price_text, 
 			e.registration_start_date, e.registration_handle_url FROM la_eb_events e
-					join la_eb_locations loc on loc.id = e.location_id
-					join la_users usr on usr.id = e.created_by
-					join la_eb_categories orga on orga.id = e.main_category_id
-			WHERE e.published = 1 AND e.id = $id" );
+					left join la_eb_locations loc on loc.id = e.location_id
+					left join la_users usr on usr.id = e.created_by
+					left join la_eb_categories orga on orga.id = e.main_category_id
+					WHERE e.published = 1 AND e.id = $id" );
 			$res  = $stmt->execute( [ $id ] );
 			$res  = $stmt->fetch( PDO::FETCH_ASSOC );
 		return $res;
