c6bfd3f0dde34f57baaa1212dd4acde0cd1eb9e8
WafflesMcDuff <goldjd@gmail.com>
2020-12-06 09:51:50 -0500

    Add orga_id to event GET endpoint

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -3,7 +3,7 @@
 class event {
 
 	public function get_all() {
-		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
+		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, e.event_date, e.event_end_date, loc.name as location_name, 
 		loc.address as location_address, e.short_description, e.short_description_en, e.event_capacity,
 		replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangements, 
 		replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre1, 
@@ -23,7 +23,7 @@ class event {
 	}
 
 	public function get( $id, $needle ) {
-			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, e.event_date, e.event_end_date, loc.name as location_name, 
+			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, e.event_date, e.event_end_date, loc.name as location_name, 
 			loc.address as location_address, e.short_description, e.short_description_en, e.event_capacity,
 			replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangements, 
 			replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre1, 
