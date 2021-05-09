e6477f0c79e2e1d4d98dfb753f9c2815a78f7e72
WafflesMcDuff <goldjd@gmail.com>
2021-05-09 11:28:37 +0200

    Resolve #2

--- includes/classes/orga.php
+++ includes/classes/orga.php
@@ -10,7 +10,15 @@ class orga {
 		$res  = $stmt->fetchAll( PDO::FETCH_ASSOC );
 		return $res;
 	}
-
+	public function get_active() {
+		$stmt = database::$conn->prepare( "SELECT orga.id, orga.name, orga.name_en, orga.email, orga.website, orga.description, orga.description_en 
+		FROM la_eb_categories orga
+		left outer join  la_eb_events e on e.main_category_id = orga.id
+		WHERE e.published = 1 AND (substring_index(e.event_date,' ',1) > CURDATE() - INTERVAL 12 MONTH)" );
+		$res  = $stmt->execute();
+		$res  = $stmt->fetchAll( PDO::FETCH_ASSOC );
+		return $res;
+	}
 	public function get( $id, $needle ) {
 			$stmt = database::$conn->prepare( "SELECT id, name, name_en, email, website, description, description_en 
             FROM joomla_la.la_eb_categories 
