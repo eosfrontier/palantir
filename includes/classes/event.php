38c4eb315a38772a15b0dccb539b59a62496a371
WafflesMcDuff <goldjd@gmail.com>
2021-05-09 11:34:42 +0200

    Resolve #1

--- includes/classes/event.php
+++ includes/classes/event.php
@@ -3,7 +3,7 @@
 class event {
 
 	public function get_all() {
-		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, e.event_date, e.event_end_date, loc.name as location_name, 
+		$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, orga.website as orga_website, e.registration_handle_url, e.event_date, e.event_end_date, loc.name as location_name, 
 		loc.address as location_address, e.short_description, e.short_description_en, e.event_capacity,
 		replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangements, 
 		replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre1, 
@@ -12,7 +12,7 @@ class event {
 		replace(json_extract(e.custom_fields,'$.min_age'),'\"','') as min_age,
 		replace(json_extract(e.custom_fields,'$.max_age'),'\"','') as max_age,
 		e.price_text, 
-		e.registration_start_date, e.registration_handle_url FROM la_eb_events e
+		e.registration_start_date FROM la_eb_events e
 				left join la_eb_locations loc on loc.id = e.location_id
 				left join la_users usr on usr.id = e.created_by
 				left join la_eb_categories orga on orga.id = e.main_category_id
@@ -23,7 +23,7 @@ class event {
 	}
 
 	public function get( $id, $needle ) {
-			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, e.event_date, e.event_end_date, loc.name as location_name, 
+			$stmt = database::$conn->prepare( "SELECT e.id, e.title, e.title_en, orga.name as organisatie, orga.name_en as organization, orga.id as orga_id, orga.website as orga_website, e.registration_handle_url, e.event_date, e.event_end_date, loc.name as location_name, 
 			loc.address as location_address, e.short_description, e.short_description_en, e.event_capacity,
 			replace(json_extract(e.custom_fields,'$.slaapen'),'\"','') as sleeping_arrangements, 
 			replace(json_extract(e.custom_fields,'$.genre'),'\"','') as genre1, 
@@ -32,7 +32,7 @@ class event {
 			replace(json_extract(e.custom_fields,'$.min_age'),'\"','') as min_age,
 			replace(json_extract(e.custom_fields,'$.max_age'),'\"','') as max_age,
 			e.price_text, 
-			e.registration_start_date, e.registration_handle_url FROM la_eb_events e
+			e.registration_start_date FROM la_eb_events e
 					left join la_eb_locations loc on loc.id = e.location_id
 					left join la_users usr on usr.id = e.created_by
 					left join la_eb_categories orga on orga.id = e.main_category_id
