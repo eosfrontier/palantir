e0ee0ea795cadb96576818860a069f6396143f85
WafflesMcDuff <goldjd@gmail.com>
2021-09-24 16:41:10 -0400

    Text replacements

--- api/v1/event/_get.php
+++ api/v1/event/_get.php
@@ -1,34 +1,34 @@
 <?php
-if ( isset( $input['all_data'] ) ) {
-	$all_data = $c_fetch->get_all();
-	if ( empty( $all_data ) ) {
-		http_response_code( 404 );
-		echo json_encode( 'None found.' );
-		die();
-	}
-	http_response_code( 200 );
-	echo json_encode( $all_data );
-	die();
+if (isset($input['all_data']) ) {
+    $all_data = $c_fetch->get_all();
+    if (empty($all_data) ) {
+        http_response_code(404);
+        echo json_encode('None found.');
+        die();
+    }
+    http_response_code(200);
+    echo $all_data;
+    die();
 }
 
 
 // CHECK BY event ID
 
-if ( isset( $input['event_id'] ) || isset( $input['id'] ) ) {
-	$eventid      = isset( $input['event_id'] ) ? $input['event_id'] : ( isset( $input['id'] ) ? $input['id'] : '' );
-	$a_event = $c_fetch->get( $eventid, 'id' );
-	if ( empty( $a_event ) ) {
-		http_response_code( 404 );
-		echo json_encode( 'None found.' );
-		die();
-	}
+if (isset($input['event_id']) || isset($input['id']) ) {
+    $eventid      = isset($input['event_id']) ? $input['event_id'] : ( isset($input['id']) ? $input['id'] : '' );
+    $a_event = $c_fetch->get($eventid, 'id');
+    if (empty($a_event) ) {
+        http_response_code(404);
+        echo json_encode('None found.');
+        die();
+    }
 
-	http_response_code( 200 );
-	echo json_encode( $a_event );
-	die();
+    http_response_code(200);
+    echo $a_event;
+    die();
 }
 
 
 // Haven't answered a way to access.
-http_response_code( 400 );
-echo json_encode( "You haven't included an 'event_id'." );
+http_response_code(400);
+echo json_encode("You haven't included an 'event_id'.");
